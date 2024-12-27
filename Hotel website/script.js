78fb4364e5b3565fe5346fc70cffb5bb2339e01f
KNL-nami <iamviu82@gmail.com>
2024-12-27 15:10:40 +0900

    up

---
+++ Hotel website/script.js
@@ -0,0 +1,137 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+
+var swiper = new Swiper(".home-slider", {
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+var swiper = new Swiper(".room-slider", {
+    spaceBetween: 20,
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+var swiper = new Swiper(".gallery-slider", {
+    spaceBetween: 10,
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
+    autoplay: {
+        delay: 1500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        991: {
+            slidesPerView: 4,
+        },
+    },
+});
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 10,
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+let accordions = document.querySelectorAll('.faqs .row .content .box');
+
+accordions.forEach(acco =>{
+    acco.onclick = () =>{
+        accordions.forEach(subAcco => {subAcco.classList.remove('active')});
+        acco.classList.add('active');
+    }
+})
+
+
+
+// 스크롤 이벤트를 감지하여 header에 클래스 추가
+/*window.addEventListener('scroll', function() {
+    const header = document.querySelector('.header');
+ 
+    if (window.scrollY > 50) { // 스크롤 위치가 50px 이상일 때
+       header.classList.add('scrolled');
+    } else {
+       header.classList.remove('scrolled');
+    }
+ });*/
+
+
+ window.addEventListener('scroll', function() {
+    const header = document.querySelector('.header');
+    const scrollPosition = window.scrollY + window.innerHeight;
+    const scrollThreshold = document.documentElement.scrollHeight * 0.3; // 페이지 높이의 30%
+
+    if (scrollPosition >= scrollThreshold) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+
+
+
+
+
+
+
+
